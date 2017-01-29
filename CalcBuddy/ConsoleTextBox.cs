37305512980f5b280d7bf5138f112991009ef2cb
timmi-on-rails <dd-tom@web.de>
2017-01-29 22:45:08 +0100

    basic example working

---
+++ CalcBuddy/ConsoleTextBox.cs
@@ -0,0 +1,102 @@
+﻿using FastColoredTextBoxNS;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CalcBuddy
+{
+    /// <summary>
+    /// Console emulator.
+    /// </summary>
+    public class ConsoleTextBox : FastColoredTextBox
+    {
+        //private volatile bool isReadLineMode;
+        private volatile bool isUpdating;
+        private Place StartReadPlace { get; set; }
+        private volatile TaskCompletionSource<bool> isReadLineModeTask = new TaskCompletionSource<bool>();
+
+        /// <summary>
+        /// Append line to end of text.
+        /// </summary>
+        /// <param name="text"></param>
+        public void WriteLine(string text)
+        {
+            isReadLineModeTask.TrySetResult(false);
+            isUpdating = true;
+            try
+            {
+                AppendText(text);
+                GoEnd();
+            }
+            finally
+            {
+                isUpdating = false;
+                ClearUndo();
+            }
+        }
+
+        /// <summary>
+        /// Wait for line entering.
+        /// Set IsReadLineMode to false for break of waiting.
+        /// </summary>
+        /// <returns></returns>
+        public async Task<string> ReadLineAsync()
+        {
+            GoEnd();
+            StartReadPlace = Range.End;
+            isReadLineModeTask = new TaskCompletionSource<bool>();
+            await isReadLineModeTask.Task;
+
+            ClearUndo();
+
+            return new Range(this, StartReadPlace, Range.End).Text.TrimEnd('\r', '\n');
+        }
+
+        public override void OnTextChanging(ref string text)
+        {
+            if (isReadLineModeTask.Task.IsCompleted && !isUpdating)
+            {
+                text = ""; //cancel changing
+                return;
+            }
+
+            if (!isReadLineModeTask.Task.IsCompleted)
+            {
+                if (Selection.Start < StartReadPlace || Selection.End < StartReadPlace)
+                    GoEnd();//move caret to entering position
+
+                if (Selection.Start == StartReadPlace || Selection.End == StartReadPlace)
+                    if (text == "\b") //backspace
+                    {
+                        text = ""; //cancel deleting of last char of readonly text
+                        return;
+                    }
+
+                if (text != null && text.Contains('\n'))
+                {
+                    text = text.Substring(0, text.IndexOf('\n') + 1);
+                    isReadLineModeTask.TrySetResult(false);
+                }
+            }
+
+            base.OnTextChanging(ref text);
+        }
+
+        public override void Clear()
+        {
+            bool oldIsReadMode = !isReadLineModeTask.Task.IsCompleted;
+
+            isReadLineModeTask.TrySetResult(false);
+            isUpdating = true;
+
+            base.Clear();
+
+            isUpdating = false;
+            if (oldIsReadMode)
+            {
+                isReadLineModeTask = new TaskCompletionSource<bool>();
+            }
+
+            StartReadPlace = Place.Empty;
+        }
+    }
+}
--- CalcBuddy/MainWindow.xaml.cs
+++ CalcBuddy/MainWindow.xaml.cs
@@ -1,28 +1,71 @@
 ﻿using FastColoredTextBoxNS;
-using System.Windows;
+using MathParser;
+
 
 namespace CalcBuddy
 {
-    public partial class MainWindow : Window
+    public partial class MainWindow : System.Windows.Window
     {
         readonly MathParser.MathParser mathParser;
+        readonly SymbolManager symbolManager;
 
         public MainWindow()
         {
             InitializeComponent();
             Loaded += MainWindow_Loaded;
             mathParser = new MathParser.MathParser();
+
+            symbolManager = new SymbolManager();
+            symbolManager.SetTrigonometrySymbols();
+            symbolManager.SetEvalSymbol();
         }
 
-        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        private async void MainWindow_Loaded(object sender, System.Windows.RoutedEventArgs e)
         {
             System.Windows.Forms.Integration.WindowsFormsHost host =
                 new System.Windows.Forms.Integration.WindowsFormsHost();
 
-            FastColoredTextBox textBox = new FastColoredTextBox();
+            ConsoleTextBox textBox = new ConsoleTextBox();
             host.Child = textBox;
 
             grid.Children.Add(host);
+
+            string text = "";
+           
+            do
+            {
+                textBox.WriteLine("> ");
+                text = await textBox.ReadLineAsync();
+                try
+                {
+                    Expression expression = mathParser.Parse(text);
+
+                    string detail = expression.ToDebug();
+                    Value result = expression.Evaluate(symbolManager);
+
+                    if (result.IsExpression)
+                    {
+                        textBox.WriteLine(string.Format("{0} = `{1}`", detail, result));
+                    }
+                    else
+                    {
+                        textBox.WriteLine(string.Format("{0} = {1}", detail, result));
+                    }
+
+                    expression.ExecuteAssignments(symbolManager);
+                }
+                catch (System.Exception exception)
+                {
+                    textBox.WriteLine(string.Format("error: {0}", exception.Message));
+                }
+                finally
+                {
+                    textBox.WriteLine("\n");
+                }
+
+            } while (text != "exit");
+
+            
         }
     }
 }
