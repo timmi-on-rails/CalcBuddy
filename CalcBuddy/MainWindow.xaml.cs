3e66afad91bcaeb584cc677ea81fd62eba5eb71d
timmi-on-rails <dd-tom@web.de>
2017-01-29 19:35:16 +0100

    initial commit

---
+++ CalcBuddy/MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using FastColoredTextBoxNS;
+using System.Windows;
+
+namespace CalcBuddy
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Loaded += MainWindow_Loaded;
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Forms.Integration.WindowsFormsHost host =
+                new System.Windows.Forms.Integration.WindowsFormsHost();
+
+            FastColoredTextBox textBox = new FastColoredTextBox();
+            host.Child = textBox;
+
+            grid.Children.Add(host);
+        }
+    }
+}
