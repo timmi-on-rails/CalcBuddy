1393ef609ea80aa44d15aab870f70d961572cbfb
Tom <dd-tom@web.de>
2018-11-18 16:46:02 +0100

    Added CodeMirror Editor -> basic demo working

--- CalcBuddy/App.cs
+++ CalcBuddy/App.cs
@@ -1,15 +1,59 @@
 ﻿using System;
+using static Retyped.dom;
+using static Retyped.codemirror;
+using System.Collections.Generic;
 
 namespace CalcBuddy
 {
-    public class App
-    {
-        public static void Main()
-        {
-			var parser = new MathParser.MathParser();
-			Console.WriteLine("Hello");
-			var result = parser.Parse("1+1");
-			Console.WriteLine(result.Evaluate());
+	public class App
+	{
+		public static void Main()
+		{
+			var parser2 = new MathParser.MathParser();
+			System.Console.WriteLine("Hello");
+			var result2 = parser2.Parse("1+1");
+			System.Console.WriteLine(result2.Evaluate());
+
+			var d1 = new HTMLTextAreaElement();
+			d1.style.height = "200px";
+			d1.style.width = "200px";
+
+			document.body.appendChild(d1);
+			var editor = CodeMirror.fromTextArea(d1, new CodeMirror.EditorConfiguration()
+			{
+				lineNumbers = true
+			});
+
+			Dictionary<double, CodeMirror.LineWidget> widgets = new Dictionary<double, CodeMirror.LineWidget>();
+
+			editor.on(Retyped.codemirror.Literals.change, (instance, change) =>
+			{
+				double line1 = change.from.line;
+				if (widgets.ContainsKey(line1))
+				{
+					widgets[line1].clear();
+
+				}
+
+				string l = instance.getDoc().getLine(line1);
+				var parser = new MathParser.MathParser();
+				try
+				{
+					var result = parser.Parse(l).Evaluate();
+					var div = new HTMLDivElement
+					{
+						textContent = result.ToString()
+					};
+
+					div.setAttribute("style", "padding: 15px; background: #dcfadc");
+
+					widgets[line1] = editor.addLineWidget(line1, div);
+
+				}
+				catch (Exception)
+				{
+				}
+			});
 		}
-    }
+	}
 }
--- Parser/Properties/AssemblyInfo.cs
+++ Parser/Properties/AssemblyInfo.cs
@@ -5,15 +5,15 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("shunting_yard")]
+[assembly: AssemblyTitle("Parser")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("shunting_yard")]
+[assembly: AssemblyProduct("Parser")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: InternalsVisibleTo("ExpressionTest")]
+[assembly: InternalsVisibleTo("Parser.Tests")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
