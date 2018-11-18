88fff8fb6bd5653f6427043d94d981abd1f599f6
Tom <dd-tom@web.de>
2018-11-18 18:38:26 +0100

    deploy

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,36 @@
 ﻿
+const version = "0.6.11";
+const cacheName = `airhorner-${version}`;
+self.addEventListener('install', e => {
+    e.waitUntil(
+      caches.open(cacheName).then(cache => {
+          return cache.addAll([
+            `/`,
+            `/index.html`,
+            `/bridge.js`,
+            `/bridge.meta.js`,
+            `/CalcBuddy.js`,
+            `/codemirror.css`,
+            `/codemirror.js`,
+            `/Parser.js`,
+            `/require.js`,
+            `/Tokenizer.js`
+          ])
+          .then(() => self.skipWaiting());
+      })
+    );
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+      caches.open(cacheName)
+        .then(cache => cache.match(event.request, { ignoreSearch: true }))
+        .then(response => {
+            return response || fetch(event.request);
+        })
+    );
+});
