d7c7b7ac92eb9d16b73fcd5d78196465ba7484a9
Tom <dd-tom@web.de>
2018-11-26 22:45:33 +0100

    separate parser specification

--- Parser/parser/ExpressionParser.cs
+++ Parser/parser/ExpressionParser.cs
@@ -1,53 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
 using Tokenizer;
 
 namespace Parser
 {
 	class ExpressionParser
 	{
-		static readonly Dictionary<TokenType, IPrefixParselet> _prefixParselets = new Dictionary<TokenType, IPrefixParselet>();
-		static readonly Dictionary<TokenType, IInfixParselet> _infixParselets = new Dictionary<TokenType, IInfixParselet>();
-
-		static ExpressionParser()
-		{
-			registerPrefixParselet(TokenType.Identifier, new VariableParselet());
-			registerPrefixParselet(TokenType.Decimal, new FloatingPointNumberParselet());
-			registerPrefixParselet(TokenType.Integer, new IntegerParselet());
-			//registerPrefixParselet(TokenType.False, new FixValueParselet(Value.Boolean(false)));
-			//registerPrefixParselet(TokenType.True, new FixValueParselet(Value.Boolean(true)));
-			registerPrefixParselet(TokenType.LeftParenthesis, new GroupParselet());
-			registerPrefixParselet(TokenType.Minus, new PrefixOperatorParselet(PrefixExpressionType.Negation, Precedences.PREFIX));
-			registerPrefixParselet(TokenType.Plus, new PrefixOperatorParselet(PrefixExpressionType.Positive, Precedences.PREFIX));
-
-			registerInfixParselet(TokenType.Exclamation, new PostfixOperatorParselet(PostfixExpressionType.Factorial, Precedences.POSTFIX));
-			registerInfixParselet(TokenType.Assignment, new AssignParselet());
-			registerInfixParselet(TokenType.Equal, new BinaryOperatorParselet(BinaryExpressionType.Equal, Precedences.COMPARISON, Associativity.Left));
-			registerInfixParselet(TokenType.NotEqual, new BinaryOperatorParselet(BinaryExpressionType.NotEqual, Precedences.COMPARISON, Associativity.Left));
-			registerInfixParselet(TokenType.Less, new BinaryOperatorParselet(BinaryExpressionType.Less, Precedences.COMPARISON, Associativity.Left));
-			registerInfixParselet(TokenType.Greater, new BinaryOperatorParselet(BinaryExpressionType.Greater, Precedences.COMPARISON, Associativity.Left));
-			registerInfixParselet(TokenType.LessOrEqual, new BinaryOperatorParselet(BinaryExpressionType.LessOrEqual, Precedences.COMPARISON, Associativity.Left));
-			registerInfixParselet(TokenType.GreaterOrEqual, new BinaryOperatorParselet(BinaryExpressionType.GreaterOrEqual, Precedences.COMPARISON, Associativity.Left));
-			registerInfixParselet(TokenType.QuestionMark, new TernaryParselet());
-			registerInfixParselet(TokenType.LeftParenthesis, new CallParselet());
-			registerInfixParselet(TokenType.Plus, new BinaryOperatorParselet(BinaryExpressionType.Addition, Precedences.SUM, Associativity.Left));
-			registerInfixParselet(TokenType.Minus, new BinaryOperatorParselet(BinaryExpressionType.Substraction, Precedences.SUM, Associativity.Left));
-			registerInfixParselet(TokenType.Star, new BinaryOperatorParselet(BinaryExpressionType.Multiplication, Precedences.PRODUCT, Associativity.Left));
-			registerInfixParselet(TokenType.Slash, new BinaryOperatorParselet(BinaryExpressionType.Division, Precedences.PRODUCT, Associativity.Left));
-			registerInfixParselet(TokenType.Pow, new BinaryOperatorParselet(BinaryExpressionType.Power, Precedences.EXPONENT, Associativity.Right));
-			registerInfixParselet(TokenType.Percent, new BinaryOperatorParselet(BinaryExpressionType.Modulo, Precedences.PRODUCT, Associativity.Left));
-		}
-
-		static void registerPrefixParselet(TokenType tokenType, IPrefixParselet prefixParselet)
-		{
-			_prefixParselets.Add(tokenType, prefixParselet);
-		}
-
-		static void registerInfixParselet(TokenType tokenType, IInfixParselet infixParselet)
-		{
-			_infixParselets.Add(tokenType, infixParselet);
-		}
-
 		public IExpression Parse(TokenStream tokenStream)
 		{
 			return ParseExpression(tokenStream, Precedences.EXPRESSION);
@@ -58,21 +15,21 @@ namespace Parser
 			Token token = tokenStream.Consume();
 
 			IPrefixParselet prefixParselet;
-			if (!_prefixParselets.TryGetValue(token.TokenType, out prefixParselet))
+			if (!ParserSpecification.Prefix.TryGetValue(token.TokenType, out prefixParselet))
 			{
 				// TODO better exception
 				throw new ArgumentException(
 					"Could not parse \"" + token.Content + "\".");
 			}
+
 			IExpression leftExpression = prefixParselet.Parse(ParseExpression, tokenStream, token);
 
 			IInfixParselet infixParselet;
 
-			while (_infixParselets.TryGetValue(tokenStream.Peek().TokenType, out infixParselet)
+			while (ParserSpecification.Infix.TryGetValue(tokenStream.Peek().TokenType, out infixParselet)
 				&& precedence < infixParselet.Precedence)
 			{
 				tokenStream.Consume();
-
 				leftExpression = infixParselet.Parse(ParseExpression, tokenStream, leftExpression);
 			}
 
---
+++ Parser/parser/ParserSpecification.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using Tokenizer;
+
+namespace Parser
+{
+	class ParserSpecification
+	{
+		public static readonly Dictionary<TokenType, IPrefixParselet> Prefix = new Dictionary<TokenType, IPrefixParselet>
+		{
+			{ TokenType.Identifier, new VariableParselet() },
+			{ TokenType.Decimal, new FloatingPointNumberParselet() },
+			{ TokenType.Integer, new IntegerParselet() },
+			{ TokenType.LeftParenthesis, new GroupParselet() },
+			{ TokenType.Minus, new PrefixOperatorParselet(PrefixExpressionType.Negation, Precedences.PREFIX) },
+			{ TokenType.Plus, new PrefixOperatorParselet(PrefixExpressionType.Positive, Precedences.PREFIX) }
+		};
+
+		public static readonly Dictionary<TokenType, IInfixParselet> Infix = new Dictionary<TokenType, IInfixParselet>
+		{
+			{ TokenType.Exclamation, new PostfixOperatorParselet(PostfixExpressionType.Factorial, Precedences.POSTFIX) },
+			{ TokenType.Assignment, new AssignParselet() },
+			{ TokenType.Equal, new BinaryOperatorParselet(BinaryExpressionType.Equal, Precedences.COMPARISON, Associativity.Left) },
+			{ TokenType.NotEqual, new BinaryOperatorParselet(BinaryExpressionType.NotEqual, Precedences.COMPARISON, Associativity.Left) },
+			{ TokenType.Less, new BinaryOperatorParselet(BinaryExpressionType.Less, Precedences.COMPARISON, Associativity.Left) },
+			{ TokenType.Greater, new BinaryOperatorParselet(BinaryExpressionType.Greater, Precedences.COMPARISON, Associativity.Left) },
+			{ TokenType.LessOrEqual, new BinaryOperatorParselet(BinaryExpressionType.LessOrEqual, Precedences.COMPARISON, Associativity.Left) },
+			{ TokenType.GreaterOrEqual, new BinaryOperatorParselet(BinaryExpressionType.GreaterOrEqual, Precedences.COMPARISON, Associativity.Left) },
+			{ TokenType.QuestionMark, new TernaryParselet() },
+			{ TokenType.LeftParenthesis, new CallParselet() },
+			{ TokenType.Plus, new BinaryOperatorParselet(BinaryExpressionType.Addition, Precedences.SUM, Associativity.Left) },
+			{ TokenType.Minus, new BinaryOperatorParselet(BinaryExpressionType.Substraction, Precedences.SUM, Associativity.Left) },
+			{ TokenType.Star, new BinaryOperatorParselet(BinaryExpressionType.Multiplication, Precedences.PRODUCT, Associativity.Left) },
+			{ TokenType.Slash, new BinaryOperatorParselet(BinaryExpressionType.Division, Precedences.PRODUCT, Associativity.Left) },
+			{ TokenType.Pow, new BinaryOperatorParselet(BinaryExpressionType.Power, Precedences.EXPONENT, Associativity.Right) },
+			{ TokenType.Percent, new BinaryOperatorParselet(BinaryExpressionType.Modulo, Precedences.PRODUCT, Associativity.Left) }
+		};
+	}
+}
